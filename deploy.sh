f47e303484ea98e02d5aa3969631a88b5ff4022c
Lee Robinson <me@leerob.io>
2024-10-05 09:54:59 -0500

    Fix

--- deploy.sh
+++ deploy.sh
@@ -25,7 +25,8 @@ sudo apt update
 sudo apt install docker-ce -y
 
 # Install Docker Compose
-sudo curl -L "https://github.com/docker/compose/releases/download/$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep tag_name | cut -d '\"' -f 4)/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo rm -f /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/v2.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
 # Ensure Docker Compose is executable and in path
