1c248e6f8e22bed745cb3ff6f9ab6b5c6db13f27
malikabdcom <66897575+MalikAbdCom@users.noreply.github.com>
2024-10-25 09:00:15 +0700

    Update deploy.sh, allow non root user with sudo privileges to execute.
    thanks for the tutorial lee!
    on previous commit, this script shows how to deploy using root user, but for some cases we want to disable root login and give another user sudo privileges to do deployment, i logged in with custom user and with previous command it will throw error like 'permission denied', this update works on my deployment.
    *sorrybadenglish:v

--- deploy.sh
+++ deploy.sh
@@ -111,7 +111,7 @@ if [ ! -f /etc/letsencrypt/ssl-dhparams.pem ]; then
 fi
 
 # Create Nginx config with reverse proxy, SSL support, rate limiting, and streaming support
-sudo cat > /etc/nginx/sites-available/myapp <<EOL
+sudo tee /etc/nginx/sites-available/myapp > /dev/null <<EOL
 limit_req_zone \$binary_remote_addr zone=mylimit:10m rate=10r/s;
 
 server {
