e05ff8f6a4f085a5dffa41b4384d72d17238e7da
Ruslan Gainutdinov <ruslan@wizecore.com>
2025-02-21 14:53:51 +0200

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -5,7 +5,7 @@ export function middleware(request: NextRequest) {
   const protectedCookie = request.cookies.get('protected');
 
   if (protectedCookie?.value !== '1') {
-    return NextResponse.redirect(new URL('/', request.url));
+    return NextResponse.redirect(new URL('/', request.headers.get('host') ?? request.url));
   }
 
   return NextResponse.next();
